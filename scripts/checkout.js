c703f226339267e0159e388775126507e69d790a
Abhinav <abhinavshaji16@gmail.com>
2023-11-26 12:29:17 +0530

    small update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { renderCheckoutHeader } from "./checkout/checkout.js";
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 

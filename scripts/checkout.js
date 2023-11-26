c5e0ac9e431d61ddd267a4bb8f2528f3be9c7a10
Abhinav <abhinavshaji16@gmail.com>
2023-11-26 12:27:02 +0530

    create a seperate function for checkout header and import it

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,7 @@
+import { renderCheckoutHeader } from "./checkout/checkout.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+
+renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
